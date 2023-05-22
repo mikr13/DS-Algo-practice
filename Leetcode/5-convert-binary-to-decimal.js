e39d66b696d401cf69270a2743b73da3103ff782
Mihir Kumar <mihirpandey.13@gmail.com>
2023-05-22 20:04:38 +0530

    minor fix

--- Leetcode/5-convert-binary-to-decimal.js
+++ Leetcode/5-convert-binary-to-decimal.js
@@ -4,7 +4,7 @@ const toDecimal = (str) => {
     const revStr = str.split('').reverse().join('')
     let i = 0;
     let dec = 0;
-    while (i < str.length) {
+    while (i < revStr.length) {
         dec += Math.pow(2, i) * revStr[i]
         i++
     }
