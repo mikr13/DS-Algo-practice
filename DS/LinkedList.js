be179ce07f8e7edefe5f1024347021b5e7915de3
Mihir Kumar <mihirpandey.13@gmail.com>
2024-08-08 09:30:41 +0530

    feat: more methods added for LinkedList

--- DS/LinkedList.js
+++ DS/LinkedList.js
@@ -11,23 +11,6 @@ export class LinkedList {
         this.head = null;
     }
 
-
-    addNode(data) {
-        let k = this.head;
-
-        const node = new Node(data);
-
-        if (this.head == null) {
-            this.head = node;
-        } else {
-            while (k.next != null) {
-                k = k.next;
-            }
-            k.next = node;
-        }
-
-    }
-
     display() {
         let k = this.head;
 
@@ -39,11 +22,57 @@ export class LinkedList {
         }
     }
 
-    removeData(data) {
+    addAtTheEnd(data) {
+        let k = this.head;
+        const node = new Node(data);
+
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            while (k.next !== null) {
+                k = k.next;
+            }
+
+            k.next = node;
+        }
+    }
+
+    addAtTheBeginning(data) {
+        const node = new Node(data);
+        node.next = this.head
+        this.head = node;
+    }
+
+    /**
+     * 
+     * @param {number} data 
+     * @param {number} position indexed from 1
+     */
+    addAtAPosition(data, position) {
+        const node = new Node(data);
+
+        if (position === 1) {
+            this.addAtTheBeginning(data)
+        } else {
+            let temp = this.head
+            let prev = null;
+            let k = 1;
+
+            while (temp !== null && k < position) {
+                k++;
+                prev = temp;
+                temp = temp.next;
+            }
+
+            prev.next = node;
+            node.next = temp;
+        }
+    }
+
+    removeByData(data) {
         let k = this.head;
 
-        while (k.next != null) {
-
+        while (k.next !== null) {
             if (k.next.data === data) {
                 k.next = k.next.next;
                 break;
@@ -51,7 +80,47 @@ export class LinkedList {
 
             k = k.next
         }
+    }
 
+    removeAtTheBeginning() {
+        this.head = this.head.next;
+    }
+
+    removeAtTheEnd() {
+        let temp = this.head
+
+        while (temp.next.next !== null) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+    }
+
+    /**
+     * 
+     * @param {number} position indexed from 1
+     */
+    removeAtAPosition(position) {
+        let temp = this.head;
+        let prev = null;
+        let k = 1;
+
+        if (position === 1) {
+            this.removeAtTheBeginning()
+        } else {
+            while (k < position & temp !== null) {
+                k++;
+                prev = temp;
+                temp = temp.next;
+            }
+
+            if (temp === null) {
+                return;
+            } else {
+                temp = temp.next
+                prev.next = temp;
+            }
+        }
     }
 
     reverse() {
@@ -59,7 +128,7 @@ export class LinkedList {
         let prev = null;
         let next = null;
 
-        while (temp != null) {
+        while (temp !== null) {
             next = temp.next;
             temp.next = prev;
             prev = temp;
@@ -68,18 +137,29 @@ export class LinkedList {
 
         this.head = prev;
     }
+
 }
 
 const linkedList = new LinkedList();
-linkedList.addNode(10)
-linkedList.addNode(12)
-linkedList.addNode(14)
-linkedList.addNode(18)
-linkedList.addNode(2)
+linkedList.addAtTheEnd(10)
+linkedList.addAtTheEnd(12)
+linkedList.addAtTheEnd(14)
+// linkedList.addAtTheEnd(18)
+// linkedList.addAtTheEnd(2)
+// linkedList.display()
+linkedList.addAtTheBeginning(1)
 linkedList.display()
-// linkedList.removeData(12)
+// linkedList.addAtTheEnd(20)
 // linkedList.display()
-// linkedList.removeData(2)
+// linkedList.addAtAPosition(25, 2)
 // linkedList.display()
+// linkedList.removeByData(12)
+// linkedList.display()
+// linkedList.removeAtTheBeginning()
+// linkedList.display()
+// linkedList.removeAtTheEnd()
+// linkedList.display()
+linkedList.removeAtAPosition(3)
+linkedList.display()
 // linkedList.reverse()
 // linkedList.display()
